060be899782b5ef637cea440583894f041819396
Nit123 <niteshkartha@yahoo.com>
2021-05-17 16:50:44 -0500

    Added mining

--- src/block.rs
+++ src/block.rs
@@ -8,20 +8,21 @@ pub struct Block {
 	pub prev_block_hash: BlockHash, //prev block hash
 	pub nonce: u64, // for mining
 	pub payload: String, // will change for transactions
+	pub difficulty: u128, // difficulty level to mine
 
 
 }
 
 impl Debug for Block {
 	fn fmt (&self, f: &mut Formatter) -> fmt::Result {
-		write!(f, "Block [{}]: {} at: {} with: {}", 
-			&self.index, &hex::encode(&self.hash), &self.timestamp, &self.payload
+		write!(f, "Block [{}]: {} at: {} with: {} nonce: {}", 
+			&self.index, &hex::encode(&self.hash), &self.timestamp, &self.payload, &self.nonce
 		)
 	}
 }
 
 impl Block { 
-	pub fn new(index: u32, timestamp: u128,  prev_block_hash: BlockHash, nonce: u64, payload: String) -> Self {
+	pub fn new(index: u32, timestamp: u128,  prev_block_hash: BlockHash, nonce: u64, payload: String, difficulty: u128) -> Self {
 		Block {
 			index, 
 			timestamp, 
@@ -29,9 +30,23 @@ impl Block {
 			prev_block_hash, 
 			nonce, 
 			payload,
+			difficulty,
 		}
 	}
+
+	pub fn mine (&mut self){
+		for nonce_attempt in 0..(u64::max_value()){
+			self.nonce = nonce_attempt;
+			let hash = self.hash();
+			if check_difficulty(&hash, self.difficulty){
+				self.hash = hash;
+				return;
+			}
+	}
 }
+}
+
+
 
 impl Hashable for Block {
 	fn bytes (&self) -> Vec<u8> {
@@ -42,7 +57,13 @@ impl Hashable for Block {
 		bytes.extend(&self.prev_block_hash);
 		bytes.extend(&u64_bytes(&self.nonce));
 		bytes.extend(self.payload.as_bytes());
+		bytes.extend(&u128_bytes(&self.difficulty));
+
 
 		bytes
 	}
+}
+
+pub fn check_difficulty (hash: &BlockHash, difficulty: u128) -> bool {
+	difficulty > difficulty_bytes_as_u128(&hash)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use blockchainlib::*;
 
 fn main() {
-	let mut block = Block::new(0, 0, vec![0; 32], 0, "Gensis block created".to_owned());
+	let mut block = Block::new(0, 0, vec![0; 32], 0, "Gensis block created".to_owned(), 0x0000FFFFFFFFFFFFFFFFFFFFFFFFFFFFF);
 	let h = block.hash();
 	block.hash = h;
 
 	println!("{:?}", &block);
 
+	block.mine();
+	println!("{:?}", &block);
 }
